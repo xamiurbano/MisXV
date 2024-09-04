ae6d82073838d8c759353c75211be0912c2ec461
xamiurbano <saviurbano@gmail.com>
2024-09-04 12:10:31 -0500

    Add files via upload

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2024,
+                month: 10,
+                day: 19,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
